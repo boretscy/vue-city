e166d3da947b319c2d672cde5f7137a25b7ff225
boretscy <boretscy@gmail.com>
2022-09-05 15:44:50 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,224 @@
+<template>
+    <div class="city-row">
+        <div class="city-col">
+            <a href="#" class="c-yablue c-h-yadarkblue text-decoration-none text-minus-minus w-100" @click.prevent="toggleList">
+                {{ title }}
+                <svg xmlns="http://www.w3.org/2000/svg" v-if="showList"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#corner-up"></use></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" v-else><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#corner-down"></use></svg>
+            </a>
+            <div class="city-question b-yagray b-radius-small text-center p-4" v-if="!sel && showQuestion">
+                <p>Ваш город {{ items[0].name }}?</p>
+                <a href="#" class="c-yawhite c-h-yawhite py-2 px-4 b-radius-small bg-yablue bg-h-yadarkblue text-decoration-none text-minus-minus text-uppercase me-2" @click.prevent="toggleItem(items[0].name, false)">Да</a>
+                <a href="#" class="c-yawhite c-h-yawhite py-2 px-4 b-radius-small bg-yayellow bg-h-yadarkyellow text-decoration-none text-minus-minus text-uppercase me-2" @click.prevent="showList = !showList">Нет</a>
+            </div>
+            <div class="city-list b-radius-small b-yagray bg-yawhite text-start overflow-hidden" v-if="showList" v-click-outside="close">
+                <a href="#" role="select-city" class="c-yablack c-h-yablackgray py-2 px-3 text-decoration-none d-block select-city-item-unselected position-relative" @click.prevent="setAll">
+                    Все города
+                    <span class="select-city-item-check">
+                        <svg xmlns="http://www.w3.org/2000/svg"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#check"></use></svg>
+                    </span>
+                </a>
+                <hr class="m-0" />
+                <a 
+                    v-for="(item, indx) in items"
+                    :key="indx"
+                    href="#" 
+                    class="c-yablack c-h-yablackgray py-2 px-3 text-decoration-none d-block position-relative"
+                    :class="{'select-city-item-selected': selected.indexOf(item.name)>=0, 'select-city-item-unselected': selected.indexOf(item.name)<0}"
+                    @click.prevent="toggleItem(item.name)"
+                    >
+                    {{ item.name }}
+                    <span class="select-city-item-check">
+                        <svg xmlns="http://www.w3.org/2000/svg"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#check"></use></svg>
+                    </span>
+                </a>
+            </div>
+        </div>
+        <div class="phone-col">
+            <a :href="'tel:+'+FormatPhoneIn(phone)" class="c-yablue c-h-yadarkblue text-decoration-none text-uppercase text-minus w-100">{{ FormatPhoneOut(phone) }}</a>
+            <a href="/cars/new/#/favorites" class="text-decoration-none ms-1" role="topmmenufavorites">
+                <svg xmlns="http://www.w3.org/2000/svg"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#favorites"></use></svg>
+            </a>
+            <a href="/cars/new/#/compare" class="text-decoration-none ms-1" role="topmmenucompare">
+                <svg xmlns="http://www.w3.org/2000/svg"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#compare"></use></svg>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            raw: document.getElementById('YAppsCity').getAttribute('items') || null,
+            sel: localStorage.getItem('YAPP_SELECTED_CITY') || null,
+            showQuestion: true,
+            showList: false,
+            externalShowList: Boolean(localStorage.getItem('YAPP_SELECTED_CITY_SHOW_LIST')) || false,
+
+            phone: window.calltouch_phone || '78612031866'
+        }
+    },
+    computed: {
+        items: function() {
+            return JSON.parse(this.raw) || []
+        },
+        selected: function() {
+            return (this.sel) ? this.sel.split(',') : []
+        },
+        title: function() {
+            let res = 'Все города'
+            switch ( this.selected.length ) {
+                case 0:
+                    res = res = 'Все города'
+                    break
+                case 1:
+                    res = this.selected[0]
+                    break
+                case this.items.length:
+                    res = 'Все города'
+                    break
+                default:
+                    res = this.selected.length+' города'
+                    break
+            }
+            return res
+        }
+    },
+    directives: {
+        clickOutside: {
+            bind(el, binding) {
+                el.addEventListener('click', e => e.stopPropagation());
+                document.body.addEventListener('click', binding.value);
+                console.log('sdlgjsdhl')
+            },
+            unbind(el, binding) {
+                document.body.removeEventListener('click', binding.value);
+            }
+        }
+    },
+    mounted: function() {
+        setInterval(() => {
+            if ( localStorage.getItem('YAPP_SELECTED_CITY') != this.sel ) this.sel = localStorage.getItem('YAPP_SELECTED_CITY')
+            if ( localStorage.getItem('YAPP_SELECTED_CITY_SHOW_LIST') != this.externalShowList ) this.externalShowList = Boolean(localStorage.getItem('YAPP_SELECTED_CITY_SHOW_LIST'))
+            if ( this.externalShowList ) this.showList = true
+        }, 100);
+    },
+    methods: {
+        toggleList() {
+            this.showList = !this.showList
+            localStorage.setItem('YAPP_SELECTED_CITY_SHOW_LIST', '')
+        },
+        toggleItem(name, flag = true) {
+            let indx = this.selected.indexOf(name)
+            if ( indx >= 0 ) {
+                this.selected.splice(indx, 1)
+            } else {
+                this.selected.push(name)
+            }
+            localStorage.setItem('YAPP_SELECTED_CITY', ((this.selected.length)?this.selected.join(','):''))
+            this.showQuestion = false
+            if ( flag ) this.toggleList()
+        },
+        setAll() {
+            let s = []
+            this.items.forEach( (i) => {
+                s.push(i.name)
+            })
+            localStorage.setItem('YAPP_SELECTED_CITY', ((s.length)?s.join(','):''))
+            this.showQuestion = false
+            this.toggleList()
+        },
+
+        close() {
+            console.log('sdgvljsdhvsk')
+        },
+        FormatPhoneOut(q) {
+            q = this.FormatPhoneIn(q);
+            return '+' + '7' + ' ' + q[1] + q[2] + q[3] + ' ' + q[4] + q[5] + q[6] + '-' + q[7] + q[8] + '-' + q[9] + q[10];
+        },
+        FormatPhoneIn(q) {
+            q = String(q).replace(/[^\d;]/g, '')
+            if ( q.length == 10 ) q = '7'+q
+            if ( q.length == 7 ) q = '7861'+q
+            return '7'+q.slice(1);
+        },
+    }
+}
+</script>
+
+<style scoped>
+.city-row {
+    display: grid;
+    justify-content: space-between;
+    align-items: center;
+    grid-template-columns: 47% 53%;
+}
+.city-question {
+	position: absolute;
+    width: 100%;
+	top: 60px;
+    z-index: 100;
+}
+.city-list {
+	position: absolute;
+    width: 100%;
+	top: 60px;
+    z-index: 100;
+	font-size: 1rem;
+}
+.city-list a.select-city-item-unselected::after {
+	content: 'Выбрать';
+	position: absolute;
+    right: 0;
+    font-size: .85rem;
+	color: var(--yamiddlegray);
+	display: none;
+    background-color: var(--yalightgray);
+    padding: 3px 10px;
+}
+.city-list a.select-city-item-selected::after {
+	content: 'Отменить выбор';
+	position: absolute;
+    right: 0;
+    font-size: .85rem;
+	color: var(--yamiddlegray);
+	display: none;
+    background-color: var(--yalightgray);
+    padding: 3px 10px;
+}
+.select-city-item-check {
+    position: absolute;
+    right: 15px;
+    width: 16px;
+    height: 16px;
+	display: none;
+}
+.select-city-item-selected .select-city-item-check {
+	display: inline-block;
+}
+.city-list a:hover {
+	background-color: var(--yalightgray);
+}
+.city-list a:hover::after {
+	display: inline-block;
+}
+.city-list svg {
+    margin-right: 10px;
+	fill: var(--yamiddlegray);
+}
+
+@media (max-width: 767px) {
+	
+	.city-question {
+		position: absolute;
+		width: 100vw;
+		top: 35px;
+		z-index: 100;
+		display: none;
+		left: 0;
+	}
+}
+</style>
